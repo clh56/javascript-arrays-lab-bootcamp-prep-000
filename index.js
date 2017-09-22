4c85f4ca975febf68c3a4505004c63af2cb47da1
ciara hudson <cchudson33@gmail.com>
2017-09-22 17:39:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 
 var kittens = ('Milo', 'Otis', 'Garfield');
 
-function destructivelyAppendKitten() {
-  kittens.unshift();
+function destructivelyAppendKitten(name) {
+  kittens.push();
   return kittens
 }
